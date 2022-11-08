e7749146f5807f0840fa322dbe44aad8034303bc
João Leite <joao.leite@arctouch.com>
2022-11-08 16:50:08 -0300

    Initial Commit

---
+++ Live Activities Test/Live_Activities_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Live_Activities_TestApp.swift
+//  Live Activities Test
+//
+//  Created by João Leite on 08/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Live_Activities_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
